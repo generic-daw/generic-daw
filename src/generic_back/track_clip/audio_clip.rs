3fb7aefc1bddfa40c49e82a41435fa7b268519a2
edwloef <edwin.frank.loeffler@gmail.com>
2024-09-27 12:53:21 +0200

    use rwlocks for positions in clips TODO improve this

--- src/generic_back/track_clip/audio_clip.rs
+++ src/generic_back/track_clip/audio_clip.rs
@@ -4,18 +4,18 @@ pub use interleaved_audio::InterleavedAudio;
 use crate::generic_back::{Arrangement, Position};
 use std::{
     cmp::Ordering,
-    sync::{atomic::Ordering::SeqCst, Arc},
+    sync::{atomic::Ordering::SeqCst, Arc, RwLock},
 };
 
 #[derive(Debug)]
 pub struct AudioClip {
     pub audio: Arc<InterleavedAudio>,
     /// the start of the clip relative to the start of the arrangement
-    global_start: Position,
+    global_start: RwLock<Position>,
     /// the end of the clip relative to the start of the arrangement
-    global_end: Position,
+    global_end: RwLock<Position>,
     /// the start of the clip relative to the start of the sample
-    clip_start: Position,
+    clip_start: RwLock<Position>,
     pub arrangement: Arc<Arrangement>,
 }
 
@@ -25,9 +25,12 @@ impl AudioClip {
 
         Self {
             audio,
-            global_start: Position::default(),
-            global_end: Position::from_interleaved_samples(samples, &arrangement.meter),
-            clip_start: Position::default(),
+            global_start: RwLock::default(),
+            global_end: RwLock::new(Position::from_interleaved_samples(
+                samples,
+                &arrangement.meter,
+            )),
+            clip_start: RwLock::default(),
             arrangement,
         }
     }
@@ -35,6 +38,8 @@ impl AudioClip {
     pub fn get_at_global_time(&self, global_time: u32) -> f32 {
         let start = self
             .global_start
+            .read()
+            .unwrap()
             .in_interleaved_samples(&self.arrangement.meter);
 
         if !&self.arrangement.meter.playing.load(SeqCst) {
@@ -44,6 +49,8 @@ impl AudioClip {
         let index = global_time - start
             + self
                 .clip_start
+                .read()
+                .unwrap()
                 .in_interleaved_samples(&self.arrangement.meter);
 
         *self
@@ -53,47 +60,53 @@ impl AudioClip {
             .unwrap_or(&0.0)
     }
 
-    pub const fn get_global_start(&self) -> Position {
-        self.global_start
+    pub fn get_global_start(&self) -> Position {
+        *self.global_start.read().unwrap()
     }
 
-    pub const fn get_global_end(&self) -> Position {
-        self.global_end
+    pub fn get_global_end(&self) -> Position {
+        *self.global_end.read().unwrap()
     }
 
-    pub const fn get_clip_start(&self) -> Position {
-        self.clip_start
+    pub fn get_clip_start(&self) -> Position {
+        *self.clip_start.read().unwrap()
     }
 
-    pub fn trim_start_to(&mut self, clip_start: Position) {
-        match self.clip_start.cmp(&clip_start) {
+    pub fn trim_start_to(&self, clip_start: Position) {
+        let cmp = self.clip_start.read().unwrap().cmp(&clip_start);
+        match cmp {
             Ordering::Less => {
-                self.global_start += clip_start - self.clip_start;
+                *self.global_start.write().unwrap() +=
+                    clip_start - *self.clip_start.read().unwrap();
             }
             Ordering::Equal => {}
             Ordering::Greater => {
-                self.global_start -= self.clip_start - clip_start;
+                *self.global_start.write().unwrap() -=
+                    *self.clip_start.read().unwrap() - clip_start;
             }
         }
-        self.clip_start = clip_start;
-        assert!(self.global_start <= self.global_end);
+        *self.clip_start.write().unwrap() = clip_start;
+        assert!(*self.global_start.read().unwrap() <= *self.global_end.read().unwrap());
     }
 
-    pub fn trim_end_to(&mut self, global_end: Position) {
-        self.global_end = global_end;
-        assert!(self.global_start <= self.global_end);
+    pub fn trim_end_to(&self, global_end: Position) {
+        *self.global_end.write().unwrap() = global_end;
+        assert!(*self.global_start.read().unwrap() <= *self.global_end.read().unwrap());
     }
 
-    pub fn move_to(&mut self, global_start: Position) {
-        match self.global_start.cmp(&global_start) {
+    pub fn move_to(&self, global_start: Position) {
+        let cmp = self.global_start.read().unwrap().cmp(&global_start);
+        match cmp {
             Ordering::Less => {
-                self.global_end += global_start - self.global_start;
+                *self.global_end.write().unwrap() +=
+                    global_start - *self.global_start.read().unwrap();
             }
             Ordering::Equal => {}
             Ordering::Greater => {
-                self.global_end -= self.global_start - global_start;
+                *self.global_end.write().unwrap() -=
+                    *self.global_start.read().unwrap() - global_start;
             }
         }
-        self.global_start = global_start;
+        *self.global_start.write().unwrap() = global_start;
     }
 }
--- src/generic_back/track_clip/midi_clip.rs
+++ src/generic_back/track_clip/midi_clip.rs
@@ -10,18 +10,18 @@ pub use midi_pattern::MidiPattern;
 use crate::generic_back::{Arrangement, Position};
 use std::{
     cmp::Ordering,
-    sync::{atomic::Ordering::SeqCst, Arc},
+    sync::{atomic::Ordering::SeqCst, Arc, RwLock},
 };
 
 #[derive(Debug)]
 pub struct MidiClip {
     pub pattern: MidiPattern,
     /// the start of the clip relative to the start of the arrangement
-    global_start: Position,
+    global_start: RwLock<Position>,
     /// the end of the clip relative to the start of the arrangement
-    global_end: Position,
+    global_end: RwLock<Position>,
     /// the start of the clip relative to the start of the pattern
-    pattern_start: Position,
+    pattern_start: RwLock<Position>,
     pub arrangement: Arc<Arrangement>,
 }
 
@@ -30,62 +30,72 @@ impl MidiClip {
         let len = pattern.len();
         Self {
             pattern,
-            global_start: Position::default(),
-            global_end: Position::from_interleaved_samples(len, &arrangement.meter),
-            pattern_start: Position::default(),
+            global_start: RwLock::default(),
+            global_end: RwLock::new(Position::from_interleaved_samples(len, &arrangement.meter)),
+            pattern_start: RwLock::default(),
             arrangement,
         }
     }
 
-    pub const fn get_global_start(&self) -> Position {
-        self.global_start
+    pub fn get_global_start(&self) -> Position {
+        *self.global_start.read().unwrap()
     }
 
-    pub const fn get_global_end(&self) -> Position {
-        self.global_end
+    pub fn get_global_end(&self) -> Position {
+        *self.global_end.read().unwrap()
     }
 
-    pub fn trim_start_to(&mut self, pattern_start: Position) {
-        match self.pattern_start.cmp(&pattern_start) {
+    pub fn trim_start_to(&self, pattern_start: Position) {
+        let cmp = self.pattern_start.read().unwrap().cmp(&pattern_start);
+        match cmp {
             Ordering::Less => {
-                self.global_start += pattern_start - self.pattern_start;
+                *self.global_start.write().unwrap() +=
+                    pattern_start - *self.pattern_start.read().unwrap();
             }
             Ordering::Equal => {}
             Ordering::Greater => {
-                self.global_start -= self.pattern_start - pattern_start;
+                *self.global_start.write().unwrap() -=
+                    *self.pattern_start.read().unwrap() - pattern_start;
             }
         }
-        self.pattern_start = pattern_start;
+        *self.pattern_start.write().unwrap() = pattern_start;
         self.pattern.dirty.store(DirtyEvent::NoteReplaced, SeqCst);
-        assert!(self.global_start <= self.global_end);
+        assert!(*self.global_start.read().unwrap() <= *self.global_end.read().unwrap());
     }
 
-    pub fn trim_end_to(&mut self, global_end: Position) {
-        self.global_end = global_end;
+    pub fn trim_end_to(&self, global_end: Position) {
+        *self.global_end.write().unwrap() = global_end;
         self.pattern.dirty.store(DirtyEvent::NoteReplaced, SeqCst);
-        assert!(self.global_start <= self.global_end);
+        assert!(*self.global_start.read().unwrap() <= *self.global_end.read().unwrap());
     }
 
-    pub fn move_to(&mut self, global_start: Position) {
-        match self.global_start.cmp(&global_start) {
+    pub fn move_to(&self, global_start: Position) {
+        let cmp = self.global_start.read().unwrap().cmp(&global_start);
+        match cmp {
             Ordering::Less => {
-                self.global_end += global_start - self.global_start;
+                *self.global_end.write().unwrap() +=
+                    global_start - *self.global_start.read().unwrap();
             }
             Ordering::Equal => {}
             Ordering::Greater => {
-                self.global_end -= self.global_start - global_start;
+                *self.global_end.write().unwrap() -=
+                    *self.global_start.read().unwrap() - global_start;
             }
         }
-        self.global_start = global_start;
+        *self.global_start.write().unwrap() = global_start;
         self.pattern.dirty.store(DirtyEvent::NoteReplaced, SeqCst);
     }
 
     pub(in crate::generic_back) fn get_global_midi(&self) -> Vec<Arc<MidiNote>> {
         let global_start = self
             .global_start
+            .read()
+            .unwrap()
             .in_interleaved_samples(&self.arrangement.meter);
         let global_end = self
             .global_end
+            .read()
+            .unwrap()
             .in_interleaved_samples(&self.arrangement.meter);
         self.pattern
             .notes
