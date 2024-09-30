ecb82787d75be792c92e8489cb33b9e84a0b9207
edwloef <edwin.frank.loeffler@gmail.com>
2024-09-30 16:02:10 +0200

    absolutely unnecessary gcd optimization lol

--- src/helpers/gcd.rs
+++ src/helpers/gcd.rs
@@ -1,13 +1,27 @@
-use std::ops::Rem;
-
-pub fn gcd<T>(x: T, y: T) -> T
-where
-    T: Copy + PartialEq + PartialOrd + Rem<Output = T> + From<u8>,
-{
-    if y == 0.into() {
-        x
-    } else {
-        let v = x % y;
-        gcd(y, v)
+/// Stein's algorithm <https://en.wikipedia.org/wiki/Binary_GCD_algorithm>
+pub fn gcd(a: u32, b: u32) -> u32 {
+    if a == 0 {
+        return b;
     }
+    if b == 0 {
+        return a;
+    }
+
+    let mut a = i64::from(a);
+    let mut b = i64::from(b);
+
+    let mut az = a.trailing_zeros();
+    let bz = b.trailing_zeros();
+    let shift = a.min(b);
+    b >>= bz;
+
+    while a != 0 {
+        a >>= az;
+        let diff = b - a;
+        az = diff.trailing_zeros();
+        b = a.min(b);
+        a = diff.abs();
+    }
+
+    (b << shift) as u32
 }
