0d71a4d38ae1f62d47a995f00d0d8379a808c46a
edwloef <edwin.frank.loeffler@gmail.com>
2024-10-05 13:55:50 +0200

    fuck stein's algorithm (fix overflowing shift)

--- src/helpers/gcd.rs
+++ src/helpers/gcd.rs
@@ -1,27 +1,7 @@
-/// Stein's algorithm <https://en.wikipedia.org/wiki/Binary_GCD_algorithm>
-pub fn gcd(a: u32, b: u32) -> u32 {
-    if a == 0 {
-        return b;
+pub fn gcd(mut a: u32, mut b: u32) -> u32 {
+    while b != 0 {
+        a %= b;
+        std::mem::swap(&mut a, &mut b);
     }
-    if b == 0 {
-        return a;
-    }
-
-    let mut a = i64::from(a);
-    let mut b = i64::from(b);
-
-    let mut az = a.trailing_zeros();
-    let bz = b.trailing_zeros();
-    let shift = a.min(b);
-    b >>= bz;
-
-    while a != 0 {
-        a >>= az;
-        let diff = b - a;
-        az = diff.trailing_zeros();
-        b = a.min(b);
-        a = diff.abs();
-    }
-
-    (b << shift) as u32
+    a
 }
