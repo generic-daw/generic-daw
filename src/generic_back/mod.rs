a2001571c9eb2519ca9bc8b94174ff5e3d0e4adb
edwloef <edwin.frank.loeffler@gmail.com>
2024-10-07 10:56:14 +0200

    slightly optimize playback

--- src/generic_back/mod.rs
+++ src/generic_back/mod.rs
@@ -42,14 +42,16 @@ pub fn build_output_stream(arrangement: Arc<Arrangement>) -> Stream {
                 no_denormals(|| {
                     for sample in data.iter_mut() {
                         *sample = arrangement
-                            .get_at_global_time(arrangement.meter.global_time.load(SeqCst))
+                            .get_at_global_time(
+                                if arrangement.meter.playing.load(SeqCst)
+                                    && !arrangement.meter.exporting.load(SeqCst)
+                                {
+                                    arrangement.meter.global_time.fetch_add(1, SeqCst)
+                                } else {
+                                    arrangement.meter.global_time.load(SeqCst)
+                                },
+                            )
                             .clamp(-1.0, 1.0);
-
-                        if arrangement.meter.playing.load(SeqCst)
-                            && !arrangement.meter.exporting.load(SeqCst)
-                        {
-                            arrangement.meter.global_time.fetch_add(1, SeqCst);
-                        }
                     }
                 });
             },
