905265cfcd906f417ac1a355d9a7c41dd11cc4d0
edwloef <edwin.frank.loeffler@gmail.com>
2025-01-15 17:34:14 +0100

    remove unnecessary annotations

--- audio_graph/src/audio_graph_node_impl.rs
+++ audio_graph/src/audio_graph_node_impl.rs
@@ -1,12 +1,12 @@
-use downcast_rs::{impl_downcast, Downcast, DowncastSend, DowncastSync};
+use downcast_rs::{impl_downcast, DowncastSync};
 use std::fmt::Debug;
 
-pub trait AudioGraphNodeImpl:
-    'static + Debug + Send + Sync + Downcast + DowncastSend + DowncastSync
-{
-    /// for the first call with a certain `buf_start_sample`, process `buf` as necessary
+pub trait AudioGraphNodeImpl: Debug + DowncastSync {
+    /// If your node has any dependencies in the audio graph, this is expected to cache its output.
     ///
-    /// for all directly consecutive calls with the same `buf_start_sample`, add the outputs from the first call
+    /// The first time this is called, `buf` will contain the summed data from all dependencies.
+    /// In any subsequent calls, don't rely on the contents of `buf`, rather just add the cached
+    /// output to `buf`.
     fn fill_buf(&self, buf_start_sample: usize, buf: &mut [f32]);
 }
 
