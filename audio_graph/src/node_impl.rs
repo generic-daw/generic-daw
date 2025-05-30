6ee2bfe3f6e16ba32a78d66f15af82d140195d07
edwloef <edwin.frank.loeffler@gmail.com>
2025-05-30 20:28:36 +0200

    remove unnecessary blanket impl for `NodeImpl`

--- audio_graph/src/node_impl.rs
+++ audio_graph/src/node_impl.rs
@@ -1,5 +1,5 @@
 use crate::NodeId;
-use std::{fmt::Debug, ops::Deref};
+use std::fmt::Debug;
 
 pub trait NodeImpl<Event>: Debug + Send {
     /// process audio data into `audio` and event data into `events`
@@ -10,25 +10,4 @@ pub trait NodeImpl<Event>: Debug + Send {
     fn reset(&self);
     /// the delay this node introduces
     fn delay(&self) -> usize;
-}
-
-impl<T, Event> NodeImpl<Event> for T
-where
-    T: Debug + Send + Deref<Target: NodeImpl<Event> + Sized>,
-{
-    fn process(&self, audio: &mut [f32], events: &mut Vec<Event>) {
-        (**self).process(audio, events);
-    }
-
-    fn id(&self) -> NodeId {
-        (**self).id()
-    }
-
-    fn reset(&self) {
-        (**self).reset();
-    }
-
-    fn delay(&self) -> usize {
-        (**self).delay()
-    }
 }
