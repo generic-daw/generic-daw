01282c9ad884d483ac40b483db8040c2a7142bec
edwloef <edwin.frank.loeffler@gmail.com>
2025-07-14 11:54:38 +0200

    add assert and more safety comment to include_f32s

--- generic_daw_utils/src/include_f32s.rs
+++ generic_daw_utils/src/include_f32s.rs
@@ -4,8 +4,11 @@ macro_rules! include_f32s {
         #[repr(align(4))]
         struct Align<T: ?Sized>(T);
         static ALIGNED: &Align<[u8]> = &Align(*::core::include_bytes!($file));
+        assert!(ALIGNED.0.len().is_multiple_of(4));
         // SAFETY:
-        // the resulting slice is correctly aligned and strictly within the original slice
+        // Every valid [u8; 4] bitpattern is a valid f32 bitpattern. The resulting
+        // slice is correctly aligned to 4 bytes and inhabits exactly the same memory
+        // as the original slice.
         unsafe { ::std::slice::from_raw_parts(ALIGNED.0.as_ptr().cast(), ALIGNED.0.len() / 4) }
     }};
 }
