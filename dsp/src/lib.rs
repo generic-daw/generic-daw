7423868c43b95fc9faa0a01c4a5147b20783a0d7
edwloef <edwin.frank.loeffler@gmail.com>
2026-02-15 17:27:08 +0100

    add smoothed f32 type

--- dsp/src/lib.rs
+++ dsp/src/lib.rs
@@ -2,8 +2,10 @@ mod allpass_comb;
 mod biquad;
 mod delay_line;
 mod lowpass_feedback_comb;
+mod smoothed_f32;
 
 pub use allpass_comb::AllpassComb;
 pub use biquad::{Biquad, BiquadCoeffs};
 pub use delay_line::DelayLine;
 pub use lowpass_feedback_comb::LowpassFeedbackComb;
+pub use smoothed_f32::SmoothedF32;
---
+++ dsp/src/smoothed_f32.rs
@@ -0,0 +1,27 @@
+#[derive(Clone, Copy, Debug, Default)]
+pub struct SmoothedF32 {
+	mem: f32,
+	goal: f32,
+	fac: f32,
+}
+
+impl SmoothedF32 {
+	#[must_use]
+	pub fn new(sample_rate: f32, value: f32) -> Self {
+		Self {
+			mem: value,
+			goal: value,
+			fac: 100.0 / sample_rate,
+		}
+	}
+
+	pub fn set(&mut self, goal: f32) {
+		self.goal = goal;
+	}
+
+	#[must_use]
+	pub fn tick(&mut self) -> f32 {
+		self.mem = self.mem.mul_add(1.0 - self.fac, self.goal * self.fac);
+		self.mem
+	}
+}
