6ab45ab4829555982cef8f4e26f0d9c38d07a80c
edwloef <edwin.frank.loeffler@gmail.com>
2026-02-24 17:27:15 +0100

    refactor automation point interpolate

--- generic_daw_core/src/automation_clip.rs
+++ generic_daw_core/src/automation_clip.rs
@@ -1,5 +1,4 @@
-use crate::{AutomationPatternId, AutomationTransition, OffsetPosition, daw_ctx::State};
-use std::f32::consts::{FRAC_PI_2, PI};
+use crate::{AutomationPatternId, OffsetPosition, daw_ctx::State};
 
 #[derive(Clone, Copy, Debug)]
 pub struct AutomationClip {
@@ -29,35 +28,7 @@ impl AutomationClip {
 			.find(|[_, next]| next.position.to_samples(&state.transport) > now)
 			.map_or_else(
 				|| pattern.points.last().unwrap().value,
-				|[this, next]| {
-					let this_time = this.position.to_samples(&state.transport);
-					let next_time = next.position.to_samples(&state.transport);
-
-					if now < this_time {
-						return this.value;
-					}
-
-					if this_time == next_time {
-						return next.value;
-					}
-
-					let amt = (now - this_time) as f32 / (next_time - this_time) as f32;
-					let linear = amt.mul_add(next.value, this.value * (1.0 - amt));
-
-					match this.to_next {
-						AutomationTransition::Linear => linear,
-						AutomationTransition::UCos(mix) => {
-							let amt = amt.mul_add(FRAC_PI_2, -FRAC_PI_2).cos();
-							let ucos = amt.mul_add(next.value, this.value * (1.0 - amt));
-							mix.mul_add(linear - ucos, linear)
-						}
-						AutomationTransition::BCos(mix) => {
-							let amt = amt.mul_add(PI, PI).cos().mul_add(0.5, 0.5);
-							let bcos = amt.mul_add(next.value, this.value * (1.0 - amt));
-							mix.mul_add(linear - bcos, linear)
-						}
-					}
-				},
+				|[this, next]| this.interpolate(next, now, &state.transport),
 			)
 	}
 }
--- generic_daw_core/src/automation_point.rs
+++ generic_daw_core/src/automation_point.rs
@@ -1,4 +1,5 @@
-use crate::MusicalTime;
+use crate::{MusicalTime, Transport};
+use std::f32::consts::{FRAC_PI_2, PI};
 
 #[derive(Clone, Copy, Debug)]
 pub enum AutomationTransition {
@@ -7,9 +8,42 @@ pub enum AutomationTransition {
 	BCos(f32),
 }
 
+impl AutomationTransition {
+	#[must_use]
+	pub fn interpolate(self, from: f32, to: f32, amt: f32) -> f32 {
+		let linear = from.mul_add(1.0 - amt, to * amt);
+		match self {
+			Self::Linear => linear,
+			Self::UCos(mix) => {
+				let amt = amt.mul_add(FRAC_PI_2, -FRAC_PI_2).cos();
+				let ucos = amt.mul_add(to, from * (1.0 - amt));
+				mix.mul_add(linear - ucos, linear)
+			}
+			Self::BCos(mix) => {
+				let amt = amt.mul_add(PI, PI).cos().mul_add(0.5, 0.5);
+				let bcos = amt.mul_add(to, from * (1.0 - amt));
+				mix.mul_add(linear - bcos, linear)
+			}
+		}
+	}
+}
+
 #[derive(Clone, Copy, Debug)]
 pub struct AutomationPoint {
 	pub value: f32,
 	pub position: MusicalTime,
 	pub to_next: AutomationTransition,
+}
+
+impl AutomationPoint {
+	#[must_use]
+	pub fn interpolate(self, next: Self, time: usize, transport: &Transport) -> f32 {
+		let self_time = self.position.to_samples(transport);
+		let next_time = next.position.to_samples(transport);
+
+		let amt = (time - self_time) as f32 / (next_time - self_time) as f32;
+
+		self.to_next
+			.interpolate(self.value, next.value, amt.clamp(0.0, 1.0))
+	}
 }
