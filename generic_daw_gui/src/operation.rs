42bf7759ad21dbd7aed1974e281ec16adb6e74cb
edwloef <edwin.frank.loeffler@gmail.com>
2026-03-28 17:57:54 +0100

    fix `scroll_into_view` operation

--- generic_daw_gui/src/operation.rs
+++ generic_daw_gui/src/operation.rs
@@ -12,29 +12,33 @@ pub fn scroll_into_view<T: MaybeSend + 'static>(
 	let child = child.into();
 
 	selector::find(scrollable.clone()).and_then(move |s| {
+		let selector::Target::Scrollable { translation, .. } = s else {
+			unreachable!();
+		};
+
 		let scrollable = scrollable.clone();
 		selector::find(child.clone()).and_then(move |c| {
 			operation::scroll_to(
 				scrollable.clone(),
 				operation::AbsoluteOffset {
 					x: c.visible_bounds()
-						.is_none_or(|bounds| bounds.width != c.bounds().width)
+						.is_none_or(|vb| vb.width != c.bounds().width)
 						.then_some(
-							c.bounds().x - s.bounds().width
-								+ if c.bounds().x < s.bounds().x {
-									0.0
+							c.bounds().x - s.bounds().x - s.bounds().width
+								+ if c.bounds().x - s.bounds().x < translation.x {
+									s.bounds().width
 								} else {
-									c.bounds().width - s.bounds().x
+									c.bounds().width
 								},
 						),
 					y: c.visible_bounds()
-						.is_none_or(|bounds| bounds.height != c.bounds().height)
+						.is_none_or(|vb| vb.height != c.bounds().height)
 						.then_some(
-							c.bounds().y - s.bounds().height
-								+ if c.bounds().y < s.bounds().y {
-									0.0
+							c.bounds().y - s.bounds().y - s.bounds().height
+								+ if c.bounds().y - s.bounds().y < translation.y {
+									s.bounds().height
 								} else {
-									c.bounds().height - s.bounds().y
+									c.bounds().height
 								},
 						),
 				},
