3564e239cdb43e62fe7d1f86db2da4bd40ce8395
edwloef <edwin.frank.loeffler@gmail.com>
2026-04-30 09:18:29 +0200

    slightly faster resampling

--- dsp/src/resample.rs
+++ dsp/src/resample.rs
@@ -41,8 +41,8 @@ pub fn resample_cubic(audio: &mut [f32], samples: &[f32], resample_ratio: f64, o
 
 fn interp_cubic(s0: f32, s1: f32, s2: f32, s3: f32, fract: f32) -> f32 {
 	let c0 = s1;
-	let c1 = (-2.0 * s0 - 3.0 * s1 + 6.0 * s2 - s3) * (1.0 / 6.0);
-	let c2 = (s0 - 2.0 * s1 + s2) * (1.0 / 2.0);
-	let c3 = (-s0 + 3.0 * s1 - 3.0 * s2 + s3) * (1.0 / 6.0);
+	let c1 = (-1.0 / 3.0) * s0 + (-1.0 / 2.0) * s1 + s2 + (-1.0 / 6.0) * s3;
+	let c2 = (1.0 / 2.0) * (s0 + s2) - s1;
+	let c3 = (1.0 / 6.0) * (s3 - s0) + (1.0 / 2.0) * (s1 - s2);
 	((c3 * fract + c2) * fract + c1) * fract + c0
 }
