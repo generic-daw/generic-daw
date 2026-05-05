fd9de82b87dd2c9556b09fd4af7a5dccdf1d11c9
edwloef <edwin.frank.loeffler@gmail.com>
2026-05-05 15:52:34 +0200

    change render keybind to ctrl+shift+r

--- generic_daw_gui/src/daw.rs
+++ generic_daw_gui/src/daw.rs
@@ -1123,7 +1123,6 @@ impl Daw {
 				Some('m') => Some(Message::ToggleMetronome),
 				Some('n') => Some(Message::NewFile),
 				Some('o') => Some(Message::OpenFileDialog),
-				Some('r') => Some(Message::RenderFileDialog),
 				Some('s') => Some(Message::SaveFile),
 				_ => None,
 			},
@@ -1132,6 +1131,7 @@ impl Daw {
 				_ => None,
 			},
 			(true, true, false, false) => match key.to_latin(physical_key) {
+				Some('r') => Some(Message::RenderFileDialog),
 				Some('s') => Some(Message::SaveAsFileDialog),
 				_ => None,
 			},
