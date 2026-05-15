abc151a6042a5872b50881962e88262b94c4da46
edwloef <edwin.frank.loeffler@gmail.com>
2026-05-15 21:02:51 +0200

    add symmetric and asymmetric transition functions

--- dsp/src/lib.rs
+++ dsp/src/lib.rs
@@ -2,8 +2,10 @@ mod biquad;
 mod delay_line;
 mod resample;
 mod smoothed_f32;
+mod transition;
 
 pub use biquad::{Biquad, BiquadCoeffs};
 pub use delay_line::DelayLine;
 pub use resample::resample_cubic;
 pub use smoothed_f32::SmoothedF32;
+pub use transition::{transition_asymmetric, transition_symmetric};
---
+++ dsp/src/transition.rs
@@ -0,0 +1,28 @@
+#[must_use]
+pub fn transition_symmetric(p: f32, x: f32, y: f32) -> f32 {
+	debug_assert!((0.0..=1.0).contains(&p));
+	debug_assert!((0.0..=1.0).contains(&x));
+	debug_assert!((0.0..=1.0).contains(&y));
+
+	let p = 1.0 - 2.0 * p;
+	transition_asymmetric(p.abs(), x, y).copysign(p) / 2.0 + 0.5
+}
+
+#[must_use]
+#[expect(clippy::many_single_char_names)]
+pub fn transition_asymmetric(p: f32, x: f32, y: f32) -> f32 {
+	debug_assert!((0.0..=1.0).contains(&p));
+	debug_assert!((0.0..=1.0).contains(&x));
+	debug_assert!((0.0..=1.0).contains(&y));
+
+	let a = 1.0 - 2.0 * x;
+	let b = 1.0 - 2.0 * y;
+
+	let t = if a.abs() < f32::EPSILON {
+		p
+	} else {
+		((x.powi(2) + a * p).sqrt() - x) / a
+	};
+
+	b * t * (t - 1.0) + t
+}
