e706c94821147db3fc80b313b48b2d2e65c96d5b
edwloef <edwin.frank.loeffler@gmail.com>
2026-09-01 11:13:51 +0200

    remove unused smoothed_f32 impl

--- dsp/src/lib.rs
+++ dsp/src/lib.rs
@@ -1,11 +1,9 @@
 mod delay_line;
 mod resample;
-mod smoothed_f32;
 mod transition;
 mod utility;
 
 pub use delay_line::DelayLine;
 pub use resample::resample_cubic;
-pub use smoothed_f32::SmoothedF32;
 pub use transition::{transition_asymmetric, transition_symmetric};
 pub use utility::{PanMode, Utility};
