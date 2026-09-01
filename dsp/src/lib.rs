d431bf2451abb1232c0b179da12d418bb757a19f
edwloef <edwin.frank.loeffler@gmail.com>
2026-09-01 11:13:17 +0200

    remove unused biquad impl

--- dsp/src/lib.rs
+++ dsp/src/lib.rs
@@ -1,11 +1,9 @@
-mod biquad;
 mod delay_line;
 mod resample;
 mod smoothed_f32;
 mod transition;
 mod utility;
 
-pub use biquad::{Biquad, BiquadCoeffs};
 pub use delay_line::DelayLine;
 pub use resample::resample_cubic;
 pub use smoothed_f32::SmoothedF32;
