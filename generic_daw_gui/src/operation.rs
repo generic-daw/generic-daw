fb38b1aa9bb0fb842ac1ed59a7c75862a7523ae4
edwloef <edwin.frank.loeffler@gmail.com>
2026-09-17 12:35:32 +0200

    don't crash when adding tracks without the mixer visible

--- generic_daw_gui/src/operation.rs
+++ generic_daw_gui/src/operation.rs
@@ -20,42 +20,45 @@ pub fn scroll_into_view<T: MaybeSend + 'static>(
 	widget::operate(operation::then(
 		operation::then(
 			operation::map(scrollable.clone().find(), move |s| {
-				(s.unwrap(), scrollable.clone(), child.clone())
+				(s, scrollable.clone(), child.clone())
 			}),
 			|(s, scrollable, child)| {
-				operation::map(child.find(), move |c| {
-					(s.clone(), c.unwrap(), scrollable.clone())
-				})
+				operation::map(child.find(), move |c| (s.clone(), c, scrollable.clone()))
 			},
 		),
 		|(s, c, scrollable)| {
-			let selector::Target::Scrollable { translation, .. } = s else {
-				panic!();
-			};
+			let t = s.as_ref().and_then(|s| match s {
+				selector::Target::Scrollable { translation, .. } => Some(translation),
+				_ => None,
+			});
 
 			scrollable::scroll_to(
 				scrollable,
 				scrollable::AbsoluteOffset {
-					x: c.visible_bounds()
-						.is_none_or(|vb| vb.width != c.bounds().width)
-						.then(|| {
-							c.bounds().x - s.bounds().x
-								+ if c.bounds().x - s.bounds().x < translation.x {
-									0.0
-								} else {
-									c.bounds().width - s.bounds().width
-								}
-						}),
-					y: c.visible_bounds()
-						.is_none_or(|vb| vb.height != c.bounds().height)
-						.then(|| {
-							c.bounds().y - s.bounds().y
-								+ if c.bounds().y - s.bounds().y < translation.y {
-									0.0
-								} else {
-									c.bounds().height - s.bounds().height
-								}
-						}),
+					x: c.as_ref().zip(s.as_ref()).zip(t).and_then(|((c, s), t)| {
+						c.visible_bounds()
+							.is_none_or(|vb| vb.width != c.bounds().width)
+							.then(|| {
+								c.bounds().x - s.bounds().x
+									+ if c.bounds().x - s.bounds().x < t.x {
+										0.0
+									} else {
+										c.bounds().width - s.bounds().width
+									}
+							})
+					}),
+					y: c.as_ref().zip(s.as_ref()).zip(t).and_then(|((c, s), t)| {
+						c.visible_bounds()
+							.is_none_or(|vb| vb.height != c.bounds().height)
+							.then(|| {
+								c.bounds().y - s.bounds().y
+									+ if c.bounds().y - s.bounds().y < t.y {
+										0.0
+									} else {
+										c.bounds().height - s.bounds().height
+									}
+							})
+					}),
 				},
 			)
 		},
